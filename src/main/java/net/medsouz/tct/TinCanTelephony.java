f19be997b846416e287735d6aa645c5a5a250fb0
medsouz <medsouz99@gmail.com>
2014-01-13 15:48:13 -0500

    Initial commit

---
+++ src/main/java/net/medsouz/tct/TinCanTelephony.java
@@ -0,0 +1,18 @@
+package net.medsouz.tct;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = TinCanTelephony.MODID, version = TinCanTelephony.VERSION)
+public class TinCanTelephony
+{
+    public static final String MODID = "TCT";
+    public static final String VERSION = "0.1";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+		
+    }
+}
