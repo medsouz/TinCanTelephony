23c420c869e1f4546b19b9668360b9b8365c8ede
medsouz <medsouz99@gmail.com>
2014-02-16 01:31:07 -0500

    Enhanced RenderHelper
    Now supports image downloading and custom UV coordinates

--- src/main/java/net/medsouz/tct/gui/RenderHelper.java
+++ src/main/java/net/medsouz/tct/gui/RenderHelper.java
@@ -2,15 +2,18 @@ package net.medsouz.tct.gui;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.ThreadDownloadImageData;
+import net.minecraft.client.renderer.texture.ITextureObject;
+import net.minecraft.client.renderer.texture.TextureManager;
 import net.minecraft.util.ResourceLocation;
 
 import org.lwjgl.opengl.GL11;
 
 /**
  * @author medsouz
- *
+ * 
  */
-public class OverlayRenderer {
+public class RenderHelper {
 
 	/**
 	 * Render an image on the screen
@@ -27,7 +30,7 @@ public class OverlayRenderer {
 	 *            Height of the image
 	 */
 	public static void drawImage(ResourceLocation image, int posX, int posY, int width, int height) {
-		drawImage(image, posX, posY, width, height, 1.0f, 1.0f, 1.0f, 1f);
+		drawImage(image, posX, posY, width, height, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f);
 	}
 
 	/**
@@ -47,7 +50,7 @@ public class OverlayRenderer {
 	 *            Transparency of image
 	 */
 	public static void drawImage(ResourceLocation image, int posX, int posY, int width, int height, float opacity) {
-		drawImage(image, posX, posY, width, height, 1.0f, 1.0f, 1.0f, opacity);
+		drawImage(image, posX, posY, width, height, 1.0f, 1.0f, 1.0f, opacity, 1.0f, 1.0f);
 	}
 
 	/**
@@ -71,18 +74,39 @@ public class OverlayRenderer {
 	 *            Blue tint
 	 * @param opacity
 	 *            Transparency of image
+	 * @param u
+	 *            U axis of texture
+	 * @param v
+	 *            V axis of texture
 	 */
-	public static void drawImage(ResourceLocation image, int posX, int posY, int width, int height, float r, float g, float b, float opacity) {
+	public static void drawImage(ResourceLocation image, int posX, int posY, int width, int height, float r, float g, float b, float opacity, float u, float v) {
 		GL11.glDisable(GL11.GL_LIGHTING);
 		GL11.glDisable(GL11.GL_FOG);
 		Tessellator tessellator = Tessellator.instance;
 		Minecraft.getMinecraft().renderEngine.bindTexture(image);
 		GL11.glColor4f(r, g, b, opacity);
 		tessellator.startDrawingQuads();
-		tessellator.addVertexWithUV(posX, posY + height, 0, 0, 1);
-		tessellator.addVertexWithUV(posX + width, posY + height, 0, 1, 1);
-		tessellator.addVertexWithUV(posX + width, posY, 0, 1, 0);
+		tessellator.addVertexWithUV(posX, posY + height, 0, 0, v);
+		tessellator.addVertexWithUV(posX + width, posY + height, 0, u, v);
+		tessellator.addVertexWithUV(posX + width, posY, 0, u, 0);
 		tessellator.addVertexWithUV(posX, posY, 0, 0, 0);
 		tessellator.draw();
+	}
+	
+	/**
+	 * @param URL Image URL
+	 * @return ResourceLocation containing the downloaded image
+	 */
+	public static ResourceLocation downloadImage(String URL) {
+		ResourceLocation r = new ResourceLocation(URL);
+        TextureManager texturemanager = Minecraft.getMinecraft().getTextureManager();
+        Object object = texturemanager.getTexture(r);
+        if (object == null)
+        {
+            object = new ThreadDownloadImageData(URL, null, null);
+            texturemanager.loadTexture(r, (ITextureObject)object);
+        }
+		return r;
+		
 	}
 }
